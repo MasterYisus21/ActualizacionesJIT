c5946fd156c559efc0703a07dd6ea90ef765f135
Felipe Villamizar <felipevipa@outlook.com>
2022-05-31 16:20:03 -0500

    Completada pestaña formulario 1

--- plataformaCentroDeConciliacion/Frontend/apiconciliaciones/src/components/ModuloSolicitud.js
+++ plataformaCentroDeConciliacion/Frontend/apiconciliaciones/src/components/ModuloSolicitud.js
@@ -13,6 +13,7 @@ function ModuloSolicitud() {
                 <a href='#'><img src='images/user.png' alt='imagen usuario' className="modulo-solicitud-small-image" /></a>
                 <a href='#'><img src='images/salida.png' alt='imagen usuario' className="modulo-solicitud-small-image" /></a>
             </div>
+            <br /><br />
             <div className='modulo-solicitud-content'>
                 <div className='modulo-solicitud-content-navbar'>
                     <a href='#' className='modulo-solicitud-content-navbar-link'>Datos Generales</a>
@@ -43,13 +44,15 @@ function ModuloSolicitud() {
                             <input type="date" className="form-control" id="fechasolicitud" name='' defaultValue={today} disabled/>
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="Numero_caso" className="form-label">ID del caso</label>
+                            <label htmlFor="Numero_caso" className="form-label">Finalidad de adquisición del servicio:</label>
                             <input type="text" className="form-control" id="Numero_caso" name='Numero_caso' placeholder="123456789" />
                         </div>
                         
                     </div>
                     <div className='modulo-solicitud-content-main-column2'>
-                        <h3>Definición del asunto juridico</h3>
+                        <div>
+                            <h3>Definición del asunto juridico</h3>
+                        </div>
                         <div className='modulo-solicitud-content-main-column2-form1'>
                             ¿Asunto juridico definible?
                             <input type="radio" id="si" name="asunto_definible" value="si" />
@@ -57,14 +60,22 @@ function ModuloSolicitud() {
                             <input type="radio" id="no" name="asunto_definible" value="no" />
                             <label htmlFor="no">NO</label><br />
                         </div>
-                        <h3>Area y Tema</h3>
+                        <br />
+                        <div>
+                            <h3>Area y Tema</h3>
+                        </div>
                         <div className='modulo-solicitud-content-main-column2-form2'>
                             <p>Area:</p>
-                            <input name=""/>
+                            <input name="" className="form-control"/>
                             <p>Tema:</p>
-                            <input name=""/>
+                            <input name="" className="form-control"/>
                             <p>Subtema:</p>
-                            <input name=""/>
+                            <input name="" className="form-control"/>
+                            <br />
+                            <button className="modulo-solicitud-content-main-column2-save-button">
+                                <img src='images/guardarIcon.png' alt='imagen guardar' className="modulo-solicitud-content-main-column2-save-button-img" />
+                                <p>GUARDAR</p>
+                            </button>
                         </div>
                     </div>
                 </div>
