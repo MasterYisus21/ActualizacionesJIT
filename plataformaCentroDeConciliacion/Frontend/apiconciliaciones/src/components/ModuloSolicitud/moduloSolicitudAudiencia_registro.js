e67ded7df7dc86cfb4b23942b6a955c22859d840
Felipevipa <felipevipa@outlook.com>
2022-06-16 10:55:21 -0500

    Arreglada ruta boton regresar en crear audiencia

--- plataformaCentroDeConciliacion/Frontend/apiconciliaciones/src/components/ModuloSolicitud/moduloSolicitudAudiencia_registro.js
+++ plataformaCentroDeConciliacion/Frontend/apiconciliaciones/src/components/ModuloSolicitud/moduloSolicitudAudiencia_registro.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './css/moduloSolicitudAudiencia_registro.css';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function ModuloSolicitudAudiencia_registro() {
 
+    const UrlParams = useParams()
 
     return (
         <div className='contenedor-principal-modulo-audiencia mt-3'>
             <div className='contenedor-boton-audiencia'>
-                <Link to='../Id_solicitud/audiencias/'> {/* Modifica la ruta al ID */}
+                <Link to={'../'+ UrlParams["Id_solicitud"] +'/audiencias/'}> {/* Modifica la ruta al ID */}
                     <button className='boton-audiencia btn btn-primary'>regresar  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z" />
                     </svg></button>
