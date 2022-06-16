d66bbf7011238d23edff5d7cd2fb5b05702ada83
Felipevipa <felipevipa@outlook.com>
2022-06-16 11:01:52 -0500

    Mejorada hoja de estilos hechos

--- plataformaCentroDeConciliacion/Frontend/apiconciliaciones/src/components/ModuloSolicitud/ModuloSolicitudHechos.js
+++ plataformaCentroDeConciliacion/Frontend/apiconciliaciones/src/components/ModuloSolicitud/ModuloSolicitudHechos.js
@@ -75,14 +75,14 @@ function ModuloSolicitudHechos() {
                 <div className='modulo-solicitud-content-main-hechos-lugar-titulo'><h6>Lugar de los hechos</h6></div>
                 <div className="mb-3">
                     <label htmlFor="Departamento" className="form-label">Departamento:</label>
-                    <select className="form-select" aria-label="Default select example" id="Departamento" name='Departamento' onChange={e => {obtenerCiudadesOpciones(e.target.value); setDepartamento(e.target.value)}} value={departamento} required>
+                    <select className="form-select form-select-sm" aria-label="Default select example" id="Departamento" name='Departamento' onChange={e => {obtenerCiudadesOpciones(e.target.value); setDepartamento(e.target.value)}} value={departamento} required>
                         <option value="" label={"Selecciona uno"} disabled></option>
                         {departamentoOpciones.map(departamento => <option key={departamento["Id"]} value={departamento["Id"]} label={departamento["Nombre"]}> </option>)}
                     </select>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="Ciudad" className="form-label">Ciudad:</label>
-                    <select className="form-select" aria-label="Default select example" id="Ciudad" name='Ciudad' value={ciudad} onChange={(e) => {setCiudad(e.target.value)}} required>
+                    <select className="form-select form-select-sm" aria-label="Default select example" id="Ciudad" name='Ciudad' value={ciudad} onChange={(e) => {setCiudad(e.target.value)}} required>
                         <option value="" label={"Selecciona uno"} disabled></option>
                         {ciudadesOpciones.map(ciudades => <option key={ciudades["Id"]} value={ciudades["Id"]} >{ciudades["Nombre"]} </option>)}
                     </select>
@@ -91,18 +91,18 @@ function ModuloSolicitudHechos() {
             <div className='modulo-solicitud-content-main-hechos-resumen'>
                 <div>
                     <label htmlFor="resumen">Resumen de los hechos:</label>
-                    <textarea className="form-control" rows="8" id="resumen" name="resumen" value={resumen} onChange={(e) => {setResumen(e.target.value)}} required></textarea>
+                    <textarea className="form-control form-control-sm" rows="7" id="resumen" name="resumen" value={resumen} onChange={(e) => {setResumen(e.target.value)}} required></textarea>
                 </div>
                 <div>
                     <label htmlFor="pretensiones">Pretensiones iniciales:</label>
-                    <textarea className="form-control" rows="8" id="pretensiones" name='pretensiones' value={pretensiones} onChange={(e) => {setPretensiones(e.target.value)}} required></textarea>
+                    <textarea className="form-control form-control-sm" rows="7" id="pretensiones" name='pretensiones' value={pretensiones} onChange={(e) => {setPretensiones(e.target.value)}} required></textarea>
                 </div>
             </div>
             <div className='modulo-solicitud-content-main-hechos-determinacion-cuantia'>
                 <div className='modulo-solicitud-content-main-hechos-lugar-titulo'><h6>Determinacion De la cuantia</h6></div>
                 <div className="mb-3" style={{display: "flex", width: "35%", justifyContent: "space-evenly"}}>
                     <label htmlFor="cuantia" className="form-label">Cuantía:</label>
-                    <input type="text" className="form-control" id="cuantia" name='cuantia' placeholder="" value={cuantia} style={{width: "60%"}} onChange={e => setCuantia(e.target.value)} disabled={cuantiaIndeterminada} required />
+                    <input type="text" className="form-control form-control-sm" id="cuantia" name='cuantia' placeholder="" value={cuantia} style={{width: "60%"}} onChange={e => setCuantia(e.target.value)} disabled={cuantiaIndeterminada} required />
                 </div>
                 <div className="form-check">
                     <input className="form-check-input" type="checkbox" checked={cuantiaIndeterminada} id="flexCheckDefault" onChange={() => {if(!cuantiaIndeterminada){setCuantia('0')}; setCuantiaIndeterminada(!cuantiaIndeterminada)}} />
