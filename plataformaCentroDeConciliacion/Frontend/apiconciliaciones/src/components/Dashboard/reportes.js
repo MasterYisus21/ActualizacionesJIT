5774d02f1066cb48ce9e6e0f7fe1eab1852de3b2
jbcastellanos <bmcastellanos3@gmail.com>
2022-06-19 14:01:19 -0500

    reportes

---
+++ plataformaCentroDeConciliacion/Frontend/apiconciliaciones/src/components/Dashboard/reportes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './css/reportes.css';
+
+
+
+
+function reportes() {
+
+    return (
+        <div className="contenedor-principal-reportes">
+            <div className="contedor-encapsulamiento-gris">
+                <div className="contenedor-opciones-reporte">
+                    hola
+                </div>
+                <div className="tabla-reportes-encapsulamiento-blanco">
+                    <div className="tabla-reportes">
+                        hola
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default reportes
