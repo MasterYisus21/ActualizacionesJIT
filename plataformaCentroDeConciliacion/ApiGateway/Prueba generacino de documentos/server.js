68e82e3da9539cedbf2db27d0801fc8d08aaf34f
jairourrego123 <jairourrego123@gmail.com>
2022-06-21 06:39:57 -0500

    Generacion rsultados

--- plataformaCentroDeConciliacion/ApiGateway/Prueba generacino de documentos/server.js
+++ plataformaCentroDeConciliacion/ApiGateway/Prueba generacino de documentos/server.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 // Load the docx file as binary content
 const content = fs.readFileSync(
-    path.resolve(__dirname, "input.docx"),
+    path.resolve(__dirname, "Acta de Conciliación.docx"),
     "binary"
 );
 
@@ -18,12 +18,26 @@ const doc = new Docxtemplater(zip, {
 });
 
 // Render the document (Replace {first_name} by John, {last_name} by Doe, ...)
+
+// contancia de no acuerdo
 doc.render({
-    first_name: "John",
-    last_name: "Doe",
-    phone: "0652455478",
-    description: "New Website",
+    nombre_apellidos_convocante:"jairo",
+    nombre_apellidos_convocados:"Juan camilo P",
+    numero_cedula_convocado:1231,
+    numero_cedula_convocante:12345,
+    barrio_convocante:"Santa L",
+    localidad_convocante:"usme",
+    barrio_convocado:"Castilla",
+    localidad_convocado:"Kenedy",
+    nombre_apellidos_conciliador:"luz helena G",
+    numero_identificacion_conciliador:145342,
+    nombre_apellidos_estudiante:"Kevin andres U",
+    fecha_hora_citacion:"16/22/2020 8:00",
+    hechos:"En esta situacion lo que se presento fue un conflicto en el que ..."
+   
 });
+let date = new Date().toDateString();
+console.log(date);
 
 const buf = doc.getZip().generate({
     type: "nodebuffer",
@@ -34,4 +48,4 @@ const buf = doc.getZip().generate({
 
 // buf is a nodejs Buffer, you can either write it to a
 // file or res.send it with express for example.
-fs.writeFileSync(path.resolve(__dirname, "output.docx"), buf);
+fs.writeFileSync(path.resolve(__dirname, "output"+3+".docx"), buf);
