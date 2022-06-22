5731ca8962890fd1fc2dfbcdf84046caaee51922
jairourrego123 <jairourrego123@gmail.com>
2022-06-22 00:50:57 -0500

    22/06 portatil

--- plataformaCentroDeConciliacion/ApiGateway/server.js
+++ plataformaCentroDeConciliacion/ApiGateway/server.js
@@ -21,13 +21,16 @@ app.post("/auth/ingresar", async (req, res) => {
         config.urlApiConciliacion + "/personas?Identificacion=" + data.username
       )
       .then((response) => {
-
+        
         if (response.data != "") {
+          
           data.rol = response.data[0].Tipo_cargo_Id;
           data.app = "CentroConciliaciones";
+          
           axios
             .post("http://127.0.0.1:4000/auth", data)
             .then(function (response) {
+              
               // console.log(response);
               // req.headers['Authorization'] = "Bearer " + response.data.token
               //res.set({ "Authorization": "Bearer " + response.data.token })
