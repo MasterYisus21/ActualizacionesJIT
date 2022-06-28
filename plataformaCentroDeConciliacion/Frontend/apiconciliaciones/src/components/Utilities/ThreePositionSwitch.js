c44382926cda04d5a3a4afa75733092104a92df9
Felipe Villamizar <felipevipa@outlook.com>
2022-06-28 08:43:25 -0500

    arregaldo modulo encuesta

--- plataformaCentroDeConciliacion/Frontend/apiconciliaciones/src/components/Utilities/ThreePositionSwitch.js
+++ plataformaCentroDeConciliacion/Frontend/apiconciliaciones/src/components/Utilities/ThreePositionSwitch.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import './css/ThreePositionSwitch.css'
 import axiosApiInstance from './axiosApiInstance'
+import config from '../../config.json'
 
 function ThreePositionSwitch({ initialValue, id }) {
     const [value, setValue] = useState(initialValue)
 
     useEffect(()=> {
         console.log(value)
-        console.log(id)        
+        console.log(id)
+
+        // http://localhost:3001/api/gateway/v1/documentos/59
+        axiosApiInstance.patch(config.apiGatewayURL + '/documentos/' + id, {"Tipo_estado_Id": value})
+        .then(response => {
+            console.log(response)
+        })
     }, [value])
 
 
