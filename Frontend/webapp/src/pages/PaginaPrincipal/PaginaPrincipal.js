5145e2508aec78e7472bba6b6100d134807ee2bd
JUAN DIEGO BENAVIDEZ CASTELLANOS <juandiegobenavidezcastellanos@A50899-87.local>
2022-11-08 11:11:11 -0500

    /expedientes

--- Frontend/webapp/src/pages/PaginaPrincipal/PaginaPrincipal.js
+++ Frontend/webapp/src/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -9,6 +9,10 @@ import './PaginaPrincipal.css'
 function PaginaPrincipal() {
   return (
     <div>
+      <svg className='rectangulo' width="719" height="57" viewBox="0 0 719 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 0H719L661.318 57H0V0Z" fill="#00460F"/>
+        <label>Hola</label>
+    </svg>
       <h4>PaginaPrincipal</h4>
       <nav>
         <ul>
@@ -22,7 +26,7 @@ function PaginaPrincipal() {
             <Link to="/consultar">Consultar</Link>
           </li>
           <li style={{listStyle: "none"}}>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/dashboard/expedientes">Dashboard</Link>
           </li>
           <li style={{listStyle: "none"}}>
             <Link to="/nothing-here">Nothing Here</Link>
