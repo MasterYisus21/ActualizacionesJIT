fa836dfda3c163a2a98856a11c1783e4778d97b7
JUAN DIEGO BENAVIDEZ CASTELLANOS <juandiegobenavidezcastellanos@A50899-87.local>
2022-11-08 13:16:13 -0500

    version movil pantalla principal

--- Frontend/webapp/src/pages/PaginaPrincipal/PaginaPrincipal.js
+++ Frontend/webapp/src/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -1,40 +1,47 @@
-import React from 'react'
+import React from "react";
 
 import { Link } from "react-router-dom";
 
 // Importing css
-import './PaginaPrincipal.css'
-
+import "./PaginaPrincipal.css";
 
 function PaginaPrincipal() {
   return (
     <div>
-      <svg className='rectangulo' width="719" height="57" viewBox="0 0 719 57" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 0H719L661.318 57H0V0Z" fill="#00460F"/>
-        <label>Hola</label>
-    </svg>
-      <h4>PaginaPrincipal</h4>
-      <nav>
-        <ul>
-          <li style={{listStyle: "none"}}>
-            <Link to="/">PaginaPrincipal</Link>
-          </li>
-          <li style={{listStyle: "none"}}>
-            <Link to="/login">Login</Link>
-          </li>
-          <li style={{listStyle: "none"}}>
-            <Link to="/consultar">Consultar</Link>
-          </li>
-          <li style={{listStyle: "none"}}>
-            <Link to="/dashboard/expedientes">Dashboard</Link>
-          </li>
-          <li style={{listStyle: "none"}}>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="">
+        <div className="logo-universidad-movil">
+          <img src={"./images/escudo_ugc.png"} alt="" />
+        </div>
+        <img
+          className="svg-rectangulo"
+          src={"./icons/rectangulo.svg"}
+          alt="journal-icon"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default PaginaPrincipal
+export default PaginaPrincipal;
+
+{
+  /* <nav>
+  <ul>
+    <li style={{listStyle: "none"}}>
+      <Link to="/">PaginaPrincipal</Link>
+    </li>
+    <li style={{listStyle: "none"}}>
+      <Link to="/login">Login</Link>
+    </li>
+    <li style={{listStyle: "none"}}>
+      <Link to="/consultar">Consultar</Link>
+    </li>
+    <li style={{listStyle: "none"}}>
+      <Link to="/dashboard/expedientes">Dashboard</Link>
+    </li>
+    <li style={{listStyle: "none"}}>
+      <Link to="/nothing-here">Nothing Here</Link>
+    </li>
+  </ul>
+</nav> */
+}
