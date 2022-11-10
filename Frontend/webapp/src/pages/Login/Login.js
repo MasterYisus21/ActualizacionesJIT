405825915acc2fcda480d5bc25fc7ee03856c930
JUAN DIEGO BENAVIDEZ CASTELLANOS <juandiegobenavidezcastellanos@A50899-87.local>
2022-11-10 14:17:08 -0500

    login responsive sin boton volver + correciones

--- Frontend/webapp/src/pages/Login/Login.js
+++ Frontend/webapp/src/pages/Login/Login.js
@@ -16,8 +16,8 @@ function Login() {
                         <img src="images/logo_universidad_texto.png" />
                     </div>
                     <form className="form-inicio-sesion">
-                      <input className="form-control rounded col" placeholder="Usuario" name='nombres' required></input>                    
-                      <input className="form-control rounded col w-100" placeholder="Contraseña" name='contraseña' type="password" required></input>                
+                      <input className="form-control rounded col btn-login" placeholder="Usuario" name='nombres' required></input>                    
+                      <input className="form-control rounded col btn-login" placeholder="Contraseña" name='contraseña' type="password" required></input>                
                     </form>
                     <button className="boton-login" form="form-inicio">Ingresar</button>
                 </div>
@@ -26,7 +26,7 @@ function Login() {
                     <div className="intro-text">
                         <div className="texto-guia">
                           <label className="h3 texto-titulo">Sistema Centros de Conciliación</label>
-                          <label className="h6 texto-parrafo">Busca ser una de las entidades en prestación de servicios sociales y jurídicos más eficaz y comprometida con la causa de las personas que a diario acuden a sus servicios, contribuyendo al desarrollo de nuestro país.
+                          <label className="texto-parrafo">Busca ser una de las entidades en prestación de servicios sociales y jurídicos más eficaz y comprometida con la causa de las personas que a diario acuden a sus servicios, contribuyendo al desarrollo de nuestro país.
                           </label>
                         </div>
                     </div>    
