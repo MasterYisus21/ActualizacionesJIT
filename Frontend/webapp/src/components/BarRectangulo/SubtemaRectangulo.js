dda6b93c171adb2c9fc2caa382f8dc746b9b0db8
JUAN DIEGO BENAVIDEZ CASTELLANOS <juandiegobenavidezcastellanos@A50899-87.local>
2022-11-15 15:25:22 -0500

    correccion alineacion texto subtemas

--- Frontend/webapp/src/components/BarRectangulo/SubtemaRectangulo.js
+++ Frontend/webapp/src/components/BarRectangulo/SubtemaRectangulo.js
@@ -7,7 +7,9 @@ export default function SubtemaRectangulo({ text, icon }) {
     return (
       <div className="icono-subtemarectangulo-movil">
         <img className="img-subtemarectangulo" src={"/icons/subtema-rectangulo.svg"} alt="journal-icon" />
-        <label className="texto-subtemarectangulo">{text}</label>
+        <div className="texto-subtemarectangulo">
+          <label>{text}</label>
+        </div>
         <img className="icon-datos-solicitante" src={"/icons/" + icon + ".png"} alt="journal-icon" />
       </div>
     );
