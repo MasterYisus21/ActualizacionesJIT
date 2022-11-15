8ce80bf8b7f33b88115509c535159f49c6878ced
yeison0797 <yeison07bermudez@gmail.com>
2022-11-15 10:43:50 -0500

    pagina

--- Frontend/webapp/src/pages/Consultar/Consultar.js
+++ Frontend/webapp/src/pages/Consultar/Consultar.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import Col from 'react-bootstrap/Col';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
 
 import { Link } from "react-router-dom";
 
@@ -8,17 +13,46 @@ import './Consultar.css'
 function Consultar() {
   return (
     <div>
-      <h4>Consultar</h4>
-      <nav>
-        <ul>
-          <li style={{listStyle: "none"}}>
-            <Link to="/">PaginaPrincipal</Link>
-          </li>
-          <li style={{listStyle: "none"}}>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className=''>
+        <div className='contenedor-rectangulo-verde'>
+          <section class="container">
+            <img
+              className="svg-rectangulo"
+              src={"./icons/rectangulo.svg"}
+              alt="journal-icon"
+            />
+            <p>Consulta tu solicitud</p>
+          </section>
+        </div>
+        <div className='contenedor-consultar-documento'>
+          <div className='rectangulo-pregunta-documento'>
+            <Row className="g-2">
+              <Col md className='seleccionable-cedula'>
+                <FloatingLabel
+                  controlId="floatingSelectGrid"
+                  label="Tipo de documento"
+                >
+                  <Form.Select className='opciones-cedula' aria-label="Floating label select example">
+                    <option>Abre el menú para ver las opciones</option>
+                    <option value="1">Cédula de ciudadanía</option>
+                    <option value="2">Cédula extranjera</option>
+                    <option value="3">Tarjeta de Identidad</option>
+                    <option value="1">Registro civil</option>
+                    <option value="2">Pasaporte</option>
+                    <option value="3">NIT</option>
+                  </Form.Select>
+                </FloatingLabel>
+              </Col>
+              <Col md className='seleccionable-cedula2'>
+                <FloatingLabel controlId="floatingInputGrid" label="Número de documento">
+                  <Form.Control type="email" placeholder="name@example.com" />
+                </FloatingLabel>
+              </Col>
+
+            </Row>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- Frontend/webapp/src/pages/PaginaPrincipal/PaginaPrincipal.js
+++ Frontend/webapp/src/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -10,13 +10,41 @@ function PaginaPrincipal() {
     <div>
       <div className="">
         <div className="logo-universidad-movil">
-          <img src={"./images/escudo_ugc.png"} alt="" />
+          <img className="imagen-logo-principal" src={"./images/logo-ugc.png"} alt="" />
         </div>
-        <img
-          className="svg-rectangulo"
-          src={"./icons/rectangulo.svg"}
-          alt="journal-icon"
-        />
+        <section class="container">
+          <img
+            className="svg-rectangulo"
+            src={"./icons/rectangulo.svg"}
+            alt="journal-icon"
+          />
+          <p>Centros de conciliación</p>
+        </section>
+        <div className="cuerpo-principal">
+          <div className="carta-solicitud">
+            <div className="contenedor-descripcion">
+              <div className="contenido-descripcion">
+                
+                <label className="titulo">Haz tu solicitud</label>
+                <label className="contenido">Aquí podrás realizar una solicitud sin ningún registro previo</label>
+              </div>
+            </div>
+            <div className="contenedor-imagen">
+              <img className="imagen_solicitud" src={"./images/imagen-solicitud.jpg"} alt="" />
+            </div>    
+          </div>
+          <div className="carta-consulta">
+            <div className="contenedor-descripcion">
+              <div className="contenido-descripcion">
+                <label className="titulo">Haz tu solicitud</label>
+                <label className="contenido">Aquí podrás consultar y hacer seguimiento de tu caso</label>
+              </div>
+            </div>
+            <div className="contenedor-imagen">
+              <img className="imagen_solicitud" src={"./images/imagen-consultar.jpg"} alt="" />
+            </div>   
+          </div>
+        </div>
       </div>
     </div>
   );
