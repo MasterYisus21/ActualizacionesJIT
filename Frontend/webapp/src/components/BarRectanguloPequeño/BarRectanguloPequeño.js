974c7f33797e8fee2b55b0febef6565d7382d450
JUAN DIEGO BENAVIDEZ CASTELLANOS <bmcastellanos3@gmail.com>
2022-11-18 15:53:21 -0500

    Expedientes falta mode resposive con las tarjetas

--- Frontend/webapp/src/components/BarRectanguloPequeño/BarRectanguloPequeño.js
+++ Frontend/webapp/src/components/BarRectanguloPequeño/BarRectanguloPequeño.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
-
 // Importing css
 import "./BarRectanguloPequeño.css";
 
---
+++ Frontend/webapp/src/components/Tarjeta/Tarjeta.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BarRectanguloPequeño } from "../BarRectanguloPequeño";
+
+
+// Importing css
+import "./Tarjeta.css";
+
+export default function Tarjeta({ text }) {
+  return (
+      <div className="tarjeta">
+        <div className="heading-tarjeta">
+            <BarRectanguloPequeño text="Caso #119"/>
+        </div>
+        <div className="body-tarjeta">
+          
+          <div>
+            <h5 className="subtitle-tarjeta">Radicado:</h5>
+            <h5>R2022102436546</h5>
+          </div>
+          <div>
+            <h5 className="subtitle-tarjeta">Fecha:</h5>
+            <h5>24/10/2022</h5>
+          </div>
+          <div>
+            <h5 className="subtitle-tarjeta">Estado:</h5>
+            <h5>Audiencia Pendiente</h5>
+          </div>
+        
+        </div>
+      </div>
+ 
+  );
+}
