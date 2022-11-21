26828dc7b815a08f75fcab1e2a3e343b7eb609e6
ANDRES FELIPE VILLAMIZAR PALACIO <AndresF.VillamizarP@ugcbogota.edu.co>
2022-11-21 15:06:24 -0500

    partially added resultado page

--- Frontend/webapp/src/pages/Dashboard/ExpedientesDetalle/Resultado/Resultado.js
+++ Frontend/webapp/src/pages/Dashboard/ExpedientesDetalle/Resultado/Resultado.js
@@ -1,8 +1,53 @@
 import React from 'react'
 
+// Importing css
+import "./Resultado.css"
+
 function Resultado() {
   return (
-    <div>Resultado</div>
+    <div className='resultado-container'>
+      <h2 className='center-text'>Resultado</h2>
+      <div className="">
+        <label htmlFor="floatingTextarea2" className="form-label h4">Resultado del caso</label>
+        <select type="text" className="form-select" id="Numero_caso" name='Numero_caso' placeholder={"Se generara automaticamente"}>
+          <option selected>Selecciona una opción</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+      <br />
+      <div className="form-floating">
+        <textarea type="text" className="form-control form-control-lg" id="floatingTextarea2" name='floatingTextarea2' style={{ height: "20rem" }} placeholder="Detalla el resultado del caso" />
+        <label htmlFor="floatingTextarea2" className="form-label h4">Detalle resultado</label>
+      </div>
+      <br />
+      <div>
+        <div className='resultado-button-container'>
+          <div>
+            <button className="resultado-button">
+              <img src='/icons/save.svg' alt='imagen guardar' className="modulo-solicitud-content-main-column2-save-button-img" />
+              <p>GUARDAR</p>
+              <img src='/icons/check-mark.svg' alt='imagen guardar' className="resultado-floating-corner" />
+            </button>
+          </div>
+          <div className='resultado-button-container-actions'>
+            <button className="resultado-button">
+              <img src='/icons/filetype-docx.svg' alt='imagen guardar' className="modulo-solicitud-content-main-column2-save-button-img" />
+              <p>DESCARGAR</p>
+            </button>
+            <button className="resultado-button">
+              <img src='/icons/upload.svg' alt='imagen guardar' className="modulo-solicitud-content-main-column2-save-button-img" />
+              <p>CARGAR</p>
+            </button>
+            <button className="resultado-button">
+              <img src='/icons/download.svg' alt='imagen guardar' className="modulo-solicitud-content-main-column2-save-button-img" />
+              <p>DESCARGAR</p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
