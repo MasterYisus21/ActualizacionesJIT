277ce5522dd97be9a91fbe7f9ee3ed3b00613c43
ANDRES FELIPE VILLAMIZAR PALACIO <AndresF.VillamizarP@ugcbogota.edu.co>
2022-11-23 15:44:40 -0500

    partially added documents

--- Frontend/webapp/src/pages/Dashboard/ExpedientesDetalle/Seguimientos/Seguimientos.js
+++ Frontend/webapp/src/pages/Dashboard/ExpedientesDetalle/Seguimientos/Seguimientos.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
+//importing css
+import "./Seguimientos.css"
+
 function Seguimientos() {
   return (
-    <div>Seguimientos</div>
+    <div className='seguimientos-container'>
+      <br />
+      <h2>Seguimientos</h2>
+      <br />
+      <div className='seguimientos-card'>
+        <h3>Seguimiento #1</h3>
+        <h4>Fecha: 23/11/2022</h4>
+        <div className='seguimientos-card-state background-color-red' />
+      </div>
+      <br />
+      <div className='seguimientos-card'>
+        <h3>Seguimiento #2</h3>
+        <h4>Fecha: 23/11/2022</h4>
+        <div className='seguimientos-card-state background-color-green' />
+      </div>
+      <br />
+      <div className='seguimientos-card' style={{ justifyContent: "center" }}>
+        <h3>+ Agregar Seguimiento</h3>
+      </div>
+    </div>
   )
 }
 
