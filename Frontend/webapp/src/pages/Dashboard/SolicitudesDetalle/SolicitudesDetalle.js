807588d7e6c0b121b4603ac0ec7482ec40400bc9
yeison0797 <yeison07bermudez@gmail.com>
2022-11-25 07:28:09 -0500

    audiencia-completo

--- Frontend/webapp/src/pages/Dashboard/SolicitudesDetalle/SolicitudesDetalle.js
+++ Frontend/webapp/src/pages/Dashboard/SolicitudesDetalle/SolicitudesDetalle.js
@@ -1,8 +1,31 @@
-import React from 'react'
+
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import './SolicitudesDetalle.css'
 
 function SolicitudesDetalle() {
   return (
-    <div>SolicitudesDetalle</div>
+    <div>
+      {['REMITIR', 'bottom', 'bottom', 'bottom'].map((nombre) => (
+        <OverlayTrigger
+          trigger="click"
+          key='bottom'
+          placement='bottom'
+          className="tooltip-detalle"
+          overlay={
+            <Popover id={`popover-positioned-bottom`} className="tooltip-detalle">
+              <Popover.Body>
+                <strong>Holy guacamole!</strong> Check this info.
+              </Popover.Body>
+            </Popover>
+          }
+        >
+          <Button className="tooltip-detalle" variant="secondary">{nombre}</Button>
+        </OverlayTrigger>
+      ))}
+    </div>
   )
 }
 
