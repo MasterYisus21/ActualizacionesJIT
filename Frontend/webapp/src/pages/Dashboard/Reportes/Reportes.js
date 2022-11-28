4c936bc0bd958edb4c89bb4064487524f5dd76ba
JUAN DIEGO BENAVIDEZ CASTELLANOS <bmcastellanos3@gmail.com>
2022-11-28 15:28:59 -0500

    Reportes y detalle solicitud

--- Frontend/webapp/src/pages/Dashboard/Reportes/Reportes.js
+++ Frontend/webapp/src/pages/Dashboard/Reportes/Reportes.js
@@ -1,8 +1,38 @@
 import React from 'react'
 
+// Impirting css
+import './Reportes.css'
+
 function Reportes() {
   return (
-    <div>Reportes</div>
+    <form className="contenedor-principal-reportes">
+      <img src={"/images/Reportes.png"}></img>
+      <div className="contedor-encapsulamiento-gris">
+
+          <div className="seleccionar-buscar">
+              <div className='seleccionar-reporte'>
+                  <label className="h3">Generar reporte</label><br></br>
+                  <label className="h6">Seleccionar reporte</label><br></br>
+                  <select className="input-reportes" required>
+                      <option value=""></option>
+                      <option value="1">Reporte de Información SNIES</option>
+                      <option value="2">Consolidado</option>
+                  </select>
+              </div>
+          </div>
+
+          <div className="fecha-inicio-finalizacion">
+              <img src={"/images/calendar.png"} className="iconos-reportes"></img>
+              <input type="date" className="input-fecha" name='fechaInicio' placeholder="Fecha de inicio" required></input>
+              <input type="date" className="input-fecha" name='fechaFinal' placeholder="Fecha de finalización" required></input>
+              <img src={"/images/sobresalir.png"} className="iconos-reportes"></img>
+          </div>
+          <div>
+              <button className="btn btn-reportes">Descargar</button>
+          </div>
+          <br />
+      </div>
+    </form>
   )
 }
 
