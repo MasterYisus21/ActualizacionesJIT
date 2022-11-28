5091cb2c1b7abd4d133835d511fc31f2aba60534
jairourrego123 <jairourrego123@gmail.com>
2022-11-28 11:19:05 -0500

    peticiones genericas

---
+++ ApiGateway/ApiGatewayExpedientes/requests/requests_error.js
@@ -0,0 +1,22 @@
+errores =(error,code=0) => {
+
+    if (error.response) {
+        //response status is an error code
+        console.log("El error ocurrio en la peticion: \n"+error.response.request.path);
+        return error.response.status
+        
+      }
+      else if (error.request) {
+        //response not received though the request was sent
+        console.log("Error al conectar con el servidor ");
+        return 500
+      }
+      else {
+        //an error occurred when setting up the request
+        console.log(error.message);
+        if(code==0){return 400} else{return code}
+        
+      }
+}
+
+module.exports = errores;
--- ApiGateway/ApiGatewayExpedientes/views/views_genericos.js
+++ ApiGateway/ApiGatewayExpedientes/views/views_genericos.js
@@ -4,74 +4,52 @@ const { response } = require("express");
 const views = {};
 
 const config = require("../config.json");
-
-// listar 
-views.ListarPaises = async (req, res) => {
-  try {
-
-
-    if (!req.query.page) {
-      req.query.page = 1;
-    }
-    if (!req.query.count) {
-      req.query.count = 10;
-    }
-    if (!req.query.ordering) {
-      req.query.ordering = "id";
-    }
-    let url =
-      config.urlExpedientes +
-      "paises?page=" +
-      req.query.page +
-      "&ordering=" +
-      req.query.ordering +
-      "&count=" +
-      req.query.count;
-    if (req.query.search) {
-      url =
-        config.urlExpedientes +
-        "paises?search=" +
-        req.query.search +
-        "&page=" +
-        req.query.page +
-        "&ordering=" +
-        req.query.ordering +
-        "&count=" +
-        req.query.count;
-    }
-
-    await await axios
-      .get(url, req.auth)
-      .then((result) => {
-        res.status(200).json(result.data);
-      })
-      .catch((err) => {
-        res.status(404).json(err);
-      });
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
-};
-
+const error = require("../requests/requests_error.js")
+const requests = require("../requests/requests_generales.js");
 
 views.GenericList = async (req, res) => {
-  axios({
-    method: req.method.toLowerCase(),
-    url: config.urlExpedientes + req.url,
-    // headers: req.headers,
-    data: req.body
-  })
-    .then(response => {
-      // console.log(response.data);
-      res.send(response.data)
+  try {
+    axios({
+      method: req.method.toLowerCase(),
+      url: config.urlExpedientes + req.url.slice(1),
+      // headers: req.headers,
+      data: req.body
     })
-    .catch(error => {
-      console.log(error);
-      res.sendStatus(500)
-    });
+      .then(result => {
+        res.status(200).json(result.data)
+      })
+      .catch(err => {
+
+        res.sendStatus(error(err))
+      });
+
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+
+
 }
+views.ListarDepartamentos = async (req, res) => {
+  try {
+    const url = config.urlExpedientes + "departamentos?pais_id=" + req.params.id
+  
+    requests.get(req, res, url, "&")
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 
 
+}
+views.ListarCiudades = async (req, res) => {
+  try {
+    const url = config.urlExpedientes + "ciudades?departamento_id=" + req.params.id2
+    requests.get(req, res, url, "&")
 
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
 module.exports = views;
