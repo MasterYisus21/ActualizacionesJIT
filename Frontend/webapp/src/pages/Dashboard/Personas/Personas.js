fcc42784ca40e074daca109543efa88cf7d6a05a
yeison0797 <yeison07bermudez@gmail.com>
2022-11-30 09:35:32 -0500

    h

--- Frontend/webapp/src/pages/Dashboard/Personas/Personas.js
+++ Frontend/webapp/src/pages/Dashboard/Personas/Personas.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Buscador, Popup } from '../../../components'
 import TarjetaPersonas from '../../../components/Tarjeta/TarjetaPersonas'
 import {useState} from 'react'
+import { axiosBasicInstanceApiSolicitudes } from '../../../helpers/axiosInstances'
 
 // import css
 import './Personas.css'
@@ -9,6 +10,25 @@ import './Personas.css'
 function Personas() {
 
   const [estado, setEstado] = useState(false);  
+  const [resultadosBusqueda, setResultadosBusqueda] = useState([]);
+
+  const search = (e) => {
+    e.preventDefault()
+    // console.log(e.target.documento.value)
+    axiosBasicInstanceApiSolicitudes({
+      method: 'get',
+      url: "/listar_personas/",
+      // headers: req.headers,
+      data: {}
+    })
+      .then(result => {
+        console.log(result.data);
+        setResultadosBusqueda(result.data.results)
+      })
+      .catch(err => {
+        console.log("error");
+      });
+  }
 
   return (
     <div className='wrapp-personas'>
@@ -16,21 +36,18 @@ function Personas() {
         <Popup setEstado={setEstado} estado={estado}/>
       }
       <div className='search-load-bar'>
-        <Buscador/>
+
+          <Buscador onSubmit={e => {search(e)}}/>
+
         <svg onClick={() => setEstado(!estado)} xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-people-fill icon-personas" viewBox="0 0 16 16">
           <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
         </svg>
         <button className='boton-cargar-personas'>Cargar Excel Estudiantes</button>
       </div>
       <div className='wrapp-tarjetas-personas'>
-        <TarjetaPersonas titulo="Juan Diego Benavidez C."/>
-        <TarjetaPersonas titulo="Juan Diego Benavidez C."/>
-        <TarjetaPersonas titulo="Juan Diego Benavidez C."/>
-        <TarjetaPersonas titulo="Juan Diego Benavidez C."/>
-        <TarjetaPersonas titulo="Juan Diego Benavidez C."/>
-        <TarjetaPersonas titulo="Juan Diego Benavidez C."/>
-        <TarjetaPersonas titulo="Juan Diego Benavidez C."/>
-        <TarjetaPersonas titulo="Juan Diego Benavidez C."/>
+        <TarjetaPersonas titulo="Juan Diego Benavidez C." correo="123" celular="12" rol="estudiante" />
+        <TarjetaPersonas titulo="Juan Diego Benavidez C." correo="123" celular="12" rol="estudiante"/>
+
         </div>
     </div>
   )
