f5886c63123b0ec6c18a7e4402ab3f778105aa53
JUAN DIEGO BENAVIDEZ CASTELLANOS <bmcastellanos3@gmail.com>
2022-12-06 09:36:18 -0500

    Registro solicitud adiccion de apoderado y correcion del drap and drop

--- Frontend/webapp/src/pages/RegistrarSolicitud/RegistrarSolicitud.js
+++ Frontend/webapp/src/pages/RegistrarSolicitud/RegistrarSolicitud.js
@@ -24,6 +24,7 @@ function RegistrarSolicitud() {
   const [estratosSocieconomicos, setEstratosSocieconomicos] = useState([])
   const [tiposPersona, setTiposPersona] = useState([])
   const [tiposDocumento, setTiposDocumento] = useState([])
+  const [openbutton, setOpenbutton] = useState(false)
 
   // function Crear_anexo() {
   //   const oldElement = document.getElementById("subir-anexo");
@@ -68,7 +69,9 @@ function RegistrarSolicitud() {
   const files = myFiles.map(file => (
     <div className="wrapp-visualizacion-anexos" key={file.path}>
       {file.path} - {file.size} bytes{" "}
-      <button className="boton-subir-anexos" onClick={removeFile(file)}>Eliminar</button>
+      <svg onClick={removeFile(file)} xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="icon-close" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+      </svg>
     </div>
   ))
 
@@ -423,6 +426,130 @@ function RegistrarSolicitud() {
                 placeholder="name@example.com"
               />
             </FloatingLabel>
+
+            <label className="subtitles-secciones">Posee apoderado</label>
+            <div className='col-detalle-solicitud'>
+              <button
+                onClick={() => setOpenbutton(true)}
+                className={openbutton ? "boton-datos-apoderado-active" : "boton-datos-apoderado"} >
+                Si
+              </button>
+              <button
+                onClick={() => setOpenbutton(false)}
+                className={openbutton ? "boton-datos-apoderado" : "boton-datos-apoderado-active"} >
+                No
+              </button>
+            </div>
+
+            {openbutton &&
+
+              <>
+                <label className="subtitles-secciones">Nombre</label>
+                <div className='col-detalle-solicitud'>
+                  <FloatingLabel controlId="floatingSelectGrid" label="Nombres ">
+                    <Form.Control
+                      className="col-inputs"
+                      type="text"
+                      placeholder="name@example.com"
+                    />
+                  </FloatingLabel>
+                  <FloatingLabel controlId="floatingInputGrid" label="Correo electrónico">
+                    <Form.Control
+                      className="col-inputs"
+                      type="text"
+                      placeholder="name@example.com"
+                    />
+                  </FloatingLabel>
+                </div>
+
+                <label className="subtitles-secciones">
+                  Identificación
+                </label>
+                <div className='col-detalle-solicitud'>
+                  <FloatingLabel
+                    controlId="floatingSelectGrid"
+                    label="Tipo de documento"
+                  >
+                    <Form.Select
+                      className="col-inputs"
+                      aria-label="Floating label select example"
+                    >
+                      <option>Abre el menú para ver las opciones</option>
+                    </Form.Select>
+                  </FloatingLabel>
+                  <FloatingLabel controlId="floatingInputGrid" label="Número de documento">
+                    <Form.Control
+                      className="col-inputs"
+                      type="text"
+                      placeholder="name@example.com"
+                    />
+                  </FloatingLabel>
+                </div>
+
+                <label className="subtitles-secciones">
+                  Fecha y lugar de expedición de documento
+                </label>
+                <div className='col-detalle-solicitud'>
+                  <FloatingLabel
+                    controlId="floatingInputGrid"
+                    label="Fecha de expedición de documento"
+                  >
+                    <Form.Control
+                      className="col-inputs"
+                      type="date"
+                      placeholder="name@example.com"
+                    />
+                  </FloatingLabel>
+                  <FloatingLabel
+                    controlId="floatingInputGrid"
+                    label="Lugar de expedición"
+                  >
+                    <Form.Control
+                      className="col-inputs"
+                      type="text"
+                      placeholder="name@example.com"
+                    />
+                  </FloatingLabel>
+                </div>
+
+                <label className="subtitles-secciones">Datos adicionales</label>
+                <div className='col-detalle-solicitud'>
+                  <FloatingLabel controlId="floatingSelectGrid" label="Tarjeta profesional ">
+                    <Form.Control
+                      className="col-inputs"
+                      type="text"
+                      placeholder="name@example.com"
+                    />
+                  </FloatingLabel>
+                  <FloatingLabel controlId="floatingInputGrid" label="correo">
+                    <Form.Control
+                      className="col-inputs"
+                      type="text"
+                      placeholder="name@example.com"
+                    />
+                  </FloatingLabel>
+                </div>
+
+                <div className='col-detalle-solicitud'>
+                  <FloatingLabel controlId="floatingSelectGrid" label="Teléfono">
+                    <Form.Control
+                      className="col-inputs"
+                      type="text"
+                      placeholder="name@example.com"
+                    />
+                  </FloatingLabel>
+                  <FloatingLabel controlId="floatingInputGrid" label="Celular">
+                    <Form.Control
+                      className="col-inputs"
+                      type="text"
+                      placeholder="name@example.com"
+                    />
+                  </FloatingLabel>
+                </div>
+
+              </>
+            }
+
           </div>
         </Collapse>
 
@@ -499,10 +626,14 @@ function RegistrarSolicitud() {
               Subir más anexos
             </button> */}
 
-            <section className="form-datos">
-              <div {...getRootProps({ className: 'dropzone' })}>
+          <section className="form-datos">
+              <div className="dropzone" {...getRootProps({ className: 'dropzone' })}>
                 <input {...getInputProps()} />
-                <h6 className="boton-subir-anexos">Subir anexos</h6>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="icon-drag-drop" viewBox="0 0 16 16">
+                  <path fillRule="evenodd" d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z" />
+                  <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
+                </svg>
+                <h4>Selecciona los archivos que desseas anexar</h4>
               </div>
               <aside className="lista-anexos">
                 {files}
