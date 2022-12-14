2d1f5579900e06bf730636e8841b8a6a46b2256d
ANDRES FELIPE VILLAMIZAR PALACIO <AndresF.VillamizarP@ugcbogota.edu.co>
2022-12-14 09:32:19 -0500

    quitar boton regresar en audiencia

--- Frontend/webapp/src/pages/Dashboard/ExpedientesDetalle/Audiencia/Audiencia.js
+++ Frontend/webapp/src/pages/Dashboard/ExpedientesDetalle/Audiencia/Audiencia.js
@@ -7,13 +7,13 @@ import Form from 'react-bootstrap/Form';
 function Audiencia() {
   return (
     <div className='contenedor-principal-modulo-audiencia'>
-      <div className='contenedor-boton-audiencia'>
-        <Link to={'../' + '/audiencias/'}> {/* Modifica la ruta al ID */}
+      {/* <div className='contenedor-boton-audiencia'>
+        <Link to={'../' + '/audiencias/'}>
           <button className='boton-audiencia btn btn-primary'>regresar  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-return-left" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z" />
           </svg></button>
         </Link>
-      </div>
+      </div> */}
       <div className='titulo-informacion-audiencia'>
         <label className='titulo-audiencia'>Sesion de Audiencia</label>
         <div className='contenedor-icono-descarga'>
