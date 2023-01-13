d329829bb2d97c3010b97d83da9c441909c5db1d
unknown <felipevipa@outlook.com>
2023-01-13 14:33:09 -0500

    added upload formato personas

--- Frontend/webapp/src/pages/Dashboard/Personas/Personas.js
+++ Frontend/webapp/src/pages/Dashboard/Personas/Personas.js
@@ -96,6 +96,26 @@ function Personas() {
       });
   }
 
+  const cargarFormatoCargarEstudiantes = (e) => {
+
+    let data = new FormData()
+    data.append('file', e.target.files[0])
+
+    axiosTokenInstanceApiExpedientes({
+      method: 'post',
+      url: "/personas/cargar",
+      // headers: req.headers,
+      headers: { "Content-Type": "multipart/form-data" },
+      data: data
+    })
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   useEffect(() => {
     setResultadosBusqueda([]);
     search()
@@ -120,8 +140,8 @@ function Personas() {
         <Buscador
           valoresBuscados={valoresBuscados}
           setValoresBuscados={setValoresBuscados}
-          filtros = {filtros}
-          setFiltros = {setFiltrosAplicados}
+          filtros={filtros}
+          setFiltros={setFiltrosAplicados}
           setPage={handlePageChange}
           required
         />
@@ -131,8 +151,14 @@ function Personas() {
         <svg onClick={() => setEstado(!estado)} xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-people-fill icon-personas" viewBox="0 0 16 16" >
           <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
         </svg>
-        
-        <button className='boton-cargar-personas'>Cargar Excel Estudiantes</button>
+
+        {/* <button className='boton-cargar-personas'>Cargar Excel Estudiantes</button> */}
+
+        <form onSubmit={e => { e.preventDefault(); e.target.document.click() }}>
+          <input type="file" name="document" style={{ display: "none" }} onChange={e => { cargarFormatoCargarEstudiantes(e) }} />
+          <button className='boton-cargar-personas'>Cargar Excel Estudiantes</button>
+        </form>
+
         <button className='boton-cargar-personas' onClick={e => descargarFormatoCargarEstudiantes()}>Descargar Formato</button>
       </div>
 
