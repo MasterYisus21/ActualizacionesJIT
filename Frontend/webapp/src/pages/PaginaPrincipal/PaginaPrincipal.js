e7370e0d36a1d82786d084ff042687e0dca8ce58
JUAN DIEGO BENAVIDEZ CASTELLANOS <bmcastellanos3@gmail.com>
2023-02-01 14:51:38 -0500

    Fix inicio de sesion

--- Frontend/webapp/src/pages/PaginaPrincipal/PaginaPrincipal.js
+++ Frontend/webapp/src/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -8,27 +8,27 @@ import "./PaginaPrincipal.css";
 
 function PaginaPrincipal() {
   return (
-      <div className="wrapp-main-pantalla-principal">
+    <div className="wrapp-main-pantalla-principal">
 
-        <div className="uno">
-          <div className="logo-universidad-movil">
-            <img className="imagen-logo-principal" src={"./images/logo-ugc.png"} alt="" />
-          </div>
-            {/* <img className="logo-icontec" src={"./images/logo-icontec.png"} alt="" /> */}
-          <div className="contenedor-rectangulo-grande">
-            <BarRectangulo text="Centros de conciliación" />
-          </div>
-          <div className="logo-pagina-escritorio">
-            <img className="imagen-logo-principal2" src={"./images/logo_universidad_texto.png"} alt="" />
-          </div>
+      <div className="uno">
+        <div className="logo-universidad-movil">
+          <img className="imagen-logo-principal" src={"./images/logo-ugc.png"} alt="" />
         </div>
+        {/* <img className="logo-icontec" src={"./images/logo-icontec.png"} alt="" /> */}
+        <div className="contenedor-rectangulo-grande">
+          <BarRectangulo text="Centro de conciliación" />
+        </div>
+        <div className="logo-pagina-escritorio">
+          <img className="imagen-logo-principal2" src={"./images/logo_universidad_texto.png"} alt="" />
+        </div>
+      </div>
 
-        <div className="dos">
+      <div className="dos">
         <div className="cuerpo-principal">
           <Link to="/registrarsolicitud" className="carta-consulta-solicitud">
             <div className="contenedor-descripcion">
-                <label className="titulo">Haz tu solicitud</label>
-                <label className="contenido">Aquí podrás consultar y hacer seguimiento de tu caso</label>
+              <label className="titulo">Haz tu solicitud</label>
+              <label className="contenido">Aquí podrás hacer tu solicitud al centro de conciliación JIT.</label>
             </div>
             <div className="contenedor-imagen">
               <img className="imagen_solicitud" src={"./images/imagen-solicitud.jpg"} alt="" />
@@ -36,8 +36,8 @@ function PaginaPrincipal() {
           </Link>
           <Link to="/consultar" className="carta-consulta-solicitud">
             <div className="contenedor-descripcion">
-                <label className="titulo">Consulta tu solicitud</label>
-                <label className="contenido">Aquí podrás consultar y hacer seguimiento de tu caso</label>
+              <label className="titulo">Consulta tu solicitud</label>
+              <label className="contenido">Aquí podrás consultar y hacer seguimiento de tu caso</label>
             </div>
             <div className="contenedor-imagen">
               <img className="imagen_solicitud" src={"./images/imagen-consultar.jpg"} alt="" />
@@ -46,7 +46,7 @@ function PaginaPrincipal() {
           <Link to="login" className="boton-ingreso">Ingresar</Link>
         </div>
       </div>
-        </div>
+    </div>
   );
 }
 
