cf18962ddfd677ab2b83a1e6c6d68a5ffe461f04
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-02-22 07:51:29 -0500

    Update models.py

--- Backend/ApiDocumentos/ApiDocumentos/apiDocumentosApp/models.py
+++ Backend/ApiDocumentos/ApiDocumentos/apiDocumentosApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Documento(models.Model):
     id = models.AutoField(primary_key=True, unique=True) # los modelos que apliquen baseModels tendran estos dos campos
-    nombre= models.CharField(max_length=150,blank=True,default="nombre", null=False)
+    nombre= models.CharField(max_length=210,blank=True,default="nombre", null=False)
     estado = models.BooleanField(default=True,blank=True,null=False)
     fecha = models.DateField( auto_now=True, auto_now_add=False , blank=False , null=False) 
     documento = models.FileField(upload_to='documentos/', max_length=220, blank=True,null=True)
