e7075b5e7e548539bd913fadbc70bde3eb770850
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-05-02 10:03:05 -0500

    Update urls.py

--- Backend/ApiSolicitudes/apiSolicitudes/apiSolicitudes/urls.py
+++ Backend/ApiSolicitudes/apiSolicitudes/apiSolicitudes/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/jit_solicitudes/v1/admin/', admin.site.urls),
 
     path('api/jit_solicitudes/v1/', include('apiSolicitudesApp.routers')), # Todas las url   deberan empezar por 'api/'
     
